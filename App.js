272e517074e03c0c817079ed4df5fed55015ebc9
Margaux Chevreux <margaux.chevreux@orange.fr>
2021-02-13 23:19:38 +0100

    Bottom Tab Design

--- App.js
+++ App.js
@@ -4,6 +4,14 @@ import { createMaterialBottomTabNavigator } from "@react-navigation/material-bot
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+import {
+  FontAwesome,
+  MaterialIcons,
+  Entypo,
+  SimpleLineIcons,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 import { Avatar } from "react-native-paper";
 
@@ -24,7 +32,51 @@ const Tab = createMaterialBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+
+          if (route.name === "Profile") {
+            iconName = focused ? (
+              <FontAwesome name="user-circle" size={22} color={color} />
+            ) : (
+              <FontAwesome name="user-circle-o" size={22} color={color} />
+            );
+          } else if (route.name === "Experiences") {
+            iconName = focused ? (
+              <MaterialIcons name="work" size={22} color={color} />
+            ) : (
+              <MaterialIcons name="work-outline" size={22} color={color} />
+            );
+          } else if (route.name === "Courses") {
+            iconName = focused ? (
+              <Entypo name="graduation-cap" size={22} color={color} />
+            ) : (
+              <SimpleLineIcons name="graduation" size={22} color={color} />
+            );
+          } else if (route.name === "Stack") {
+            iconName = focused ? (
+              <Ionicons name="code-slash" size={22} color={color} />
+            ) : (
+              <Ionicons name="code-slash-outline" size={22} color={color} />
+            );
+          } else if (route.name === "Hobbies") {
+            iconName = focused ? (
+              <MaterialCommunityIcons name="emoticon-lol" size={24} color={color} />
+            ) : (
+              <MaterialCommunityIcons name="emoticon-lol-outline" size={24} color={color} />
+            );
+          }
+
+          return <Text> {iconName} </Text>;
+        },
+      })}
+      activeColor="#E8C518"
+      inactiveColor="#FFFEFA"
+      barStyle={{ backgroundColor: "#694fad" }}
+      sceneAnimationEnabled={true}
+    >
       <Tab.Screen name="Profile" component={ProfileScreen} />
       <Tab.Screen name="Experiences" component={ExperiencesScreen} />
       <Tab.Screen name="Courses" component={CoursesScreen} />
