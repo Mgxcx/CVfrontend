7a5d22f2711d341affa999db0b93b99ad00fa3dd
Margaux Chevreux <margaux.chevreux@orange.fr>
2021-02-17 20:32:50 +0100

    Deleting test for backgrounds

--- screens/StackScreen.js
+++ screens/StackScreen.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { View, KeyboardAvoidingView, ScrollView, StyleSheet, Text, ImageBackground, Dimensions } from "react-native";
-import Svg, { Path } from "react-native-svg";
-
-const WIDTH = Dimensions.get("screen").width;
+import { View, KeyboardAvoidingView, ScrollView, StyleSheet, Text, ImageBackground } from "react-native";
 
 function StackScreen() {
   return (
     <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={styles.container}>
       <ImageBackground source={require("../assets/stack.jpg")} style={styles.image}>
-        <Text style={styles.text}>Inside</Text>
+        <Text style={styles.text}>Stack</Text>
       </ImageBackground>
     </KeyboardAvoidingView>
   );
@@ -17,12 +14,16 @@ function StackScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#C7F7E7",
+    justifyContent: "center",
   },
   image: {
     marginTop: 140,
     width: "100%",
     height: "60%",
+  },
+  text: {
+    textAlign: "center",
   },
 });
 
