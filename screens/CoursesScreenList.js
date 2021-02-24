3cb295acf7bd76a53208b7d81069b068a03cba56
Margaux Chevreux <margaux.chevreux@orange.fr>
2021-02-25 00:28:56 +0100

    Designing Courses Details,Animating: transition between List and Details

--- screens/CoursesScreenList.js
+++ screens/CoursesScreenList.js
@@ -1,32 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { View, KeyboardAvoidingView, ScrollView, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
-// import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 import { Card } from "react-native-shadow-cards";
 import { connect } from "react-redux";
+import { SharedElement } from "react-navigation-shared-element";
+import { Entypo } from "@expo/vector-icons";
 
 const CoursesScreenList = ({ navigation, onSubmitCoursePosition }) => {
   let courses = [
     {
-      image: require("../assets/lacapsulebootcamp.png"),
-      title: "TITRE DÉVELOPPEUSE WEB ET MOBILE FULLSTACK JAVASCRIPT",
+      id: 1,
+      image: require("../assets/lacapsule2.jpg"),
+      icon: "graduation-cap",
+      title: "TITRE DÉVELOPPEUSE WEB\nET MOBILE FULLSTACK JAVASCRIPT",
       name: "La Capsule Academy",
       date: "Septembre 2020 à Janvier 2021",
     },
     {
-      image: require("../assets/ucl.png"),
-      title: "MASTER AFFAIRES INTERNATIONALES TRILINGUES PARCOURS TOURISME",
+      id: 2,
+      image: require("../assets/ucl.jpg"),
+      icon: "graduation-cap",
+      title: "MASTER AFFAIRES INTERNATIONALES\nTRILINGUES PARCOURS TOURISME",
       name: "Université Catholique de Lille",
       date: "Septembre 2014 à Septembre 2016",
     },
     {
-      image: require("../assets/umlv.svg.png"),
-      title: "LICENCE PROFESSIONNELLE TOURISME ET NOUVELLES TECHNOLOGIES",
-      name: "Université Paris-Est Marne-la-Vallée",
+      id: 3,
+      image: require("../assets/upemlv-ifis.jpg"),
+      icon: "graduation-cap",
+      title: "LICENCE PROFESSIONNELLE\nTOURISME ET NOUVELLES TECHNOLOGIES",
+      name: "Université Paris-Est Marne-la-Vallée (IFIS)",
       date: "Septembre 2013 à Septembre 2014",
     },
     {
-      image: require("../assets/umlv.svg.png"),
-      title: "LICENCE EN LANGUES ÉTRANGÈRES APPLIQUÉES",
+      id: 4,
+      image: require("../assets/upemlv.jpg"),
+      icon: "graduation-cap",
+      title: "LICENCE EN LANGUES ÉTRANGÈRES\nAPPLIQUÉES",
       name: "Université Paris-Est Marne-la-Vallée",
       date: "Septembre 2011 à Juin 2013",
     },
@@ -36,17 +45,22 @@ const CoursesScreenList = ({ navigation, onSubmitCoursePosition }) => {
       <View style={styles.container}>
         {courses.map((course, i) => (
           <TouchableOpacity
-            key={i}
+            key={course.id}
             onPress={() => {
-              navigation.navigate("CoursesScreenDetails");
+              navigation.navigate("CoursesScreenDetails", { course });
               onSubmitCoursePosition(i);
             }}
           >
             <Card style={styles.card}>
-              <Image source={course.image} style={styles.image} />
-              <Text style={styles.title}>{course.title}</Text>
-              <Text style={styles.text}>{course.name}</Text>
-              <Text style={styles.date}>{course.date}</Text>
+              <SharedElement id={`course.${course.image}`}>
+                <Image source={course.image} style={styles.image} />
+              </SharedElement>
+              <SharedElement id={`course.${course.title}`}>
+                <View style={{ flexDirection: "row", justifyContent: "center" }}>
+                  <Entypo name={course.icon} size={22} color="#4b93a0" style={styles.icon} />
+                  <Text style={styles.title}>{course.title}</Text>
+                </View>
+              </SharedElement>
             </Card>
           </TouchableOpacity>
         ))}
@@ -76,11 +90,14 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     padding: 10,
     borderRadius: 20,
-    backgroundColor: "#6dcaa4",
+    backgroundColor: "#C7F7E7",
+  },
+  icon: {
+    marginRight: 5,
   },
   image: {
     width: "100%",
-    height: 140,
+    height: 130,
     marginBottom: 20,
     borderRadius: 5,
   },
@@ -94,7 +111,7 @@ const styles = StyleSheet.create({
   text: {
     textAlign: "center",
     marginBottom: 5,
-    color: "#C7F7E7",
+    color: "#4b93a0",
     fontWeight: "900",
     fontSize: 14,
   },
